316a0ffa5172bf650b187251db0d6df4c68f1d6f
Jason Park <parkjs814@gmail.com>
2016-10-16 03:38:56 -0500

    fix index

--- routes/index.js
+++ routes/index.js
@@ -40,7 +40,7 @@ router.get('*', function (req, res, next) {
   res.redirect('/profile');
 });
 
-router.get('/check', function (req, res, next) {
+router.get('/($|check)', function (req, res, next) {
   res.render('check', {title: 'Medifact - Check Combinations', verb: 'will take'});
 });
 
